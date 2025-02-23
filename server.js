b744ad5df4552513f7ae7019501c48b7fac88743
Ayush Sameer Shah <120619309+aaaayushhh@users.noreply.github.com>
2025-02-23 18:06:13 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1640,7 +1640,6 @@ app.get('/Get-orders-data-user-side/:userId', async (req, res) => {
                 u.FirstName,
                 u.LastName,
                 u.CompanyName,
-                ofiles.ShippingStatus
                 GROUP_CONCAT(ofiles.ShippingStatus SEPARATOR ', ') AS ShippingStatus
             FROM 
                 Orders o
