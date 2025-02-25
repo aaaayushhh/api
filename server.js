33a9be6e3dd0db1c5197816bd611ef0281c6f787
Ayush Sameer Shah <120619309+aaaayushhh@users.noreply.github.com>
2025-02-26 00:14:48 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -147,13 +147,14 @@ require("dotenv").config();
 const authMiddleware = require('./auth');
 const nodemailer = require("nodemailer");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: "*",
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization"
 }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Middleware
 app.use(bodyParser.json());
