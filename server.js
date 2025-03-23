fa9916dbc30388590760bbf9c985315a9e810605
Ayush Sameer Shah <120619309+aaaayushhh@users.noreply.github.com>
2025-03-23 15:44:01 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -155,7 +155,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("uploads"));
+
+// ✅ Serve static files from the "uploads" folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Middleware
 app.use(bodyParser.json());
